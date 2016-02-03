1b2ada312df10c5df2fb88983867b40ed258396c
Phil Fluegel <pfluegelcx@gmail.com>
2016-02-03 15:22:17 -0600

    non working yet

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,6 +2,16 @@ class API::EventsController < ApplicationController
 
   skip_before_action :verify_authenticity_token
 
+  before_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
+
   def create
     registered_application = RegisteredApplication.find_by(url: request.env[:HTTP_ORIGIN])
     if registered_application == nil
@@ -13,6 +23,10 @@ class API::EventsController < ApplicationController
       else
         render json: {errors: @event.errors}, status: :unprocessable_entity
       end
+    end
+
+    def prefight
+      head 200
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
 
   resources :applications
 
+
   get 'welcome/index'
 
   root to: "welcome#index"
 
   namespace :api, defaults: {format: :json} do
+    match '/events', to: 'events#preflight', via: [:options]
     resources :events, only: [create]
   end
 end
